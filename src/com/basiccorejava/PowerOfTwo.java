e9d51c61ecde6986ccb14c61b56cafd659e48482
moin21 <mamoinuddin@gmail.com>
2022-02-16 21:39:11 +0530

    Added: Class containing program for power of from 0 to N

---
+++ src/com/basiccorejava/PowerOfTwo.java
@@ -0,0 +1,21 @@
+package com.basiccorejava;
+
+import java.util.Scanner;
+
+import static java.lang.StrictMath.pow;
+
+public class PowerOfTwo {
+	public static void main(String[] args) {
+		System.out.println("Enter any positive number upto 30");
+		Scanner scanner = new Scanner(System.in);
+
+		int N = scanner.nextInt();
+		PowOfTwo(N);
+	}
+
+	public static void PowOfTwo(int N) {
+		for (int i = 0; i <= N; i++) {
+			System.out.println(pow(2, i));
+		}
+	}
+}
