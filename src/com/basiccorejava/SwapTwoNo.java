73adcdcf47ee1e8d71d7278c541c5c704101651f
moin21 <mamoinuddin@gmail.com>
2022-02-16 23:25:03 +0530

    Added: Class containing logic to swap to no.

---
+++ src/com/basiccorejava/SwapTwoNo.java
@@ -0,0 +1,25 @@
+package com.basiccorejava;
+
+import java.util.*;
+
+class SwapTwoNo {
+	public static int x, y, z;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value of X");
+		x = sc.nextInt();
+		System.out.println("Enter the value of X");
+		y = sc.nextInt();
+		sc.close();
+		swappingNo(x, y, z);
+		
+	}
+	public static void swappingNo(int x, int y, int z) {
+		System.out.println("before swapping " + x + ", " + y);
+		z = x;
+		x = y;
+		y = z;
+		System.out.println("After swapping: " + x + ", " + y);
+		System.out.println();
+	}
+}
